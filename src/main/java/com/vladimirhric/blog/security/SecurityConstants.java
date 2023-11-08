57f42a2373863cd94ae766ab2ea898262045fb35
Vladimír <hric.vladimir14@gmail.com>
2023-11-08 13:34:45 +0100

    added custom .properties file for secrets and utilizing it in code (for better security, making the secret separate from source code)

--- src/main/java/com/vladimirhric/blog/security/SecurityConstants.java
+++ src/main/java/com/vladimirhric/blog/security/SecurityConstants.java
@@ -1,12 +1,22 @@
 package com.vladimirhric.blog.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
-import org.springframework.core.env.Environment;
 
 
 @Component
+@PropertySource({
+        "classpath:/secret.properties"
+})
 public class SecurityConstants {
     public static final long JWT_EXPIRATION = 70000;
-    public static final String JWT_SECRET = "ThzQOdBzozqHcvWvsfMWnXGWwhqsMOghduyasgdDGSYUGDhdsuiah7641789789dysahuidasuidhasui";
+
+
+    // loading secret constant from application.properties
+    public static String JWT_SECRET;
+    @Value("${jwt.secret}")
+    public void setNameStatic(String name){
+        SecurityConstants.JWT_SECRET = name;
+    }
 }
